877e199a4b7842d0665744114c61c7788d4b9016
Sourav Hossain <32600240+souravhossain@users.noreply.github.com>
2019-03-19 08:43:25 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**  
+ * Project Name: Appon
+ * Author Name: Sourav Hossain
+ * Email: souravhossain421@gmail.com
+ **/
+
+$(document).ready(function () {
+    $('.js-preloader').preloadinator({
+        minTime: 2000
+    });
+});
